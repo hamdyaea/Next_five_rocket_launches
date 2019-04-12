14e2c34db72f468dae61783fa9a94d3654ec31b5
hamdyaea <hamdy.aea@gmail.com>
2019-04-12 17:40:44 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+![Awesome](awesome.svg) ![Python](python.png)  
+
+# Next fice rocket launches
+
+![Screenshot](screenshot.png)
+
+## What's this software ?  
+
+This python 3 software download and display the next five rocket launches.
+
+This software is for aerospace enthusiast who do not want to miss a live rocket launch.
+
+## How to launch this software :  
+
+```sh
+python3 RocketLaunch.py
+```  
+
+## Requirements :
+
+Python 3
+
+## Developer - Author
+
+Hamdy Abou El Anein
+
+## Homepage
+
+http://www.daylightlinux.ch 
---
+++ RocketLaunch.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+# Developer : Hamdy Abou El Anein
+
+import json
+import urllib.request
+
+def main():
+    urlData = ("https://launchlibrary.net/1.4/launch/next/5")
+    webURL = urllib.request.urlopen(urlData)
+    data = webURL.read()
+    encoding = webURL.info().get_content_charset('utf-8')
+    launch = json.loads(data.decode(encoding))
+
+    print("\n--- Next five rocket launches ---\n\n")
+    try:
+        print(("Name : ")+str(launch["launches"][0]["name"])+str("\n"))
+    except:
+        print(("Name : ")+str("no name"))
+        pass
+    try:
+        print(("Description : ")+str(launch["launches"][0]["missions"][0]["description"])+str("\n"))
+    except:
+        print(("Description : ")+str("no description")+str("\n"))
+    try:
+        print(("Start time : ")+str(launch["launches"][0]["windowstart"])+str("\n"))
+    except:
+        print(("Start time : ")+str("no windowstart")+str("\n"))
+    print("\n\n")
+    try:
+        print(("Name : ")+str(launch["launches"][1]["name"])+str("\n"))
+    except:
+        print(("Name : ")+str("no name"))
+        pass
+    try:
+        print(("Description : ")+str(launch["launches"][1]["missions"][0]["description"])+str("\n"))
+    except:
+        print(("Description : ")+str("no description")+str("\n"))
+    try:
+        print(("Start time : ")+str(launch["launches"][1]["windowstart"])+str("\n"))
+    except:
+        print(("Start time : ")+str("no windowstart")+str("\n"))
+    print("\n\n")
+    try:
+        print(("Name : ")+str(launch["launches"][2]["name"])+str("\n"))
+    except:
+        print(("Name : ")+str("no name"))
+        pass
+    try:
+        print(("Description : ")+str(launch["launches"][2]["missions"][0]["description"])+str("\n"))
+    except:
+        print(("Description : ")+str("no description")+str("\n"))
+    try:
+        print(("Start time : ")+str(launch["launches"][2]["windowstart"])+str("\n"))
+    except:
+        print(("Start time : ")+str("no windowstart")+str("\n"))
+    print("\n\n")
+    try:
+        print(("Name : ")+str(launch["launches"][3]["name"])+str("\n"))
+    except:
+        print(("Name : ")+str("no name"))
+        pass
+    try:
+        print(("Description : ")+str(launch["launches"][3]["missions"][0]["description"])+str("\n"))
+    except:
+        print(("Description : ")+str("no description")+str("\n"))
+    try:
+        print(("Start time : ")+str(launch["launches"][3]["windowstart"])+str("\n"))
+    except:
+        print(("Start time : ")+str("no windowstart")+str("\n"))
+    print("\n\n")
+    try:
+        print(("Name : ")+str(launch["launches"][4]["name"])+str("\n"))
+    except:
+        print(("Name : ")+str("no name"))
+        pass
+    try:
+        print(("Description : ")+str(launch["launches"][4]["missions"][0]["description"])+str("\n"))
+    except:
+        print(("Description : ")+str("no description")+str("\n"))
+    try:
+        print(("Start time : ")+str(launch["launches"][4]["windowstart"])+str("\n"))
+    except:
+        print(("Start time : ")+str("no windowstart")+str("\n"))
+    print("\n\n")
+    print("Developer - Author : Hamdy Abou El Anein\nhttps://github.com/hamdyaea")
+
+main()
