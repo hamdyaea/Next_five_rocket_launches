e07be84048ab5dc0fbed3341da119486f01bdf83
hamdyaea <hamdy.aea@protonmail.com>
2022-01-18 16:59:45 +0100

    new api

--- RocketLaunch.py
+++ RocketLaunch.py
@@ -5,153 +5,39 @@
 
 # This Python3 software print in the terminal the next five rocket launch. The data is updated at every run.
 
-
-class Rocket:
-    def __init__(self):
-        self.launch
-
-
+from urllib.request import Request, urlopen
 import json
-import urllib.request
 
 
 def main():
-    urlData = "https://launchlibrary.net/1.4/launch/next/5"
-    webURL = urllib.request.urlopen(urlData)
-    data = webURL.read()
-    encoding = webURL.info().get_content_charset("utf-8")
-    Rocket.launch = json.loads(data.decode(encoding))
-    print(
-        "  _   _           _     _____   _____            _        _     _                            _ "
-    )
-    print(
-        " | \ | |         | |   | ____| |  __ \          | |      | |   | |                          | |"
-    )
-    print(
-        " |  \| | _____  _| |_  | |__   | |__) |___   ___| | _____| |_  | |     __ _ _   _ _ __   ___| |__ "
-    )
-    print(
-        " | . ` |/ _ \ \/ / __| |___ \  |  _  // _ \ / __| |/ / _ \ __| | |    / _` | | | | '_ \ / __| '_ \ "
-    )
-    print(
-        " | |\  |  __/>  <| |_   ___) | | | \ \ (_) | (__|   <  __/ |_  | |___| (_| | |_| | | | | (__| | | | "
-    )
-    print(
-        " |_| \_|\___/_/\_\\__|  |____/  |_|  \_\___/ \___|_|\_\___|\__| |______\__,_|\__,_|_| |_|\___|_| |_|"
-    )
+    req = Request("https://ll.thespacedevs.com/2.2.0/launch/?format=json&last_updated__gte=&last_updated__lte=&mission__orbit__name=&mission__orbit__name__icontains=&name=&net__gt=&net__gte=&net__lt=&net__lte=&r_spacex_api_id=&rocket__configuration__full_name=&rocket__configuration__full_name__icontains=&rocket__configuration__id=&rocket__configuration__manufacturer__name=&rocket__configuration__manufacturer__name__icontains=&rocket__configuration__name=&rocket__spacecraftflight__spacecraft__id=&rocket__spacecraftflight__spacecraft__name=&rocket__spacecraftflight__spacecraft__name__icontains=&slug=&status=1&window_end__gt=&window_end__gte=&window_end__lt=&window_end__lte=&window_start__gt=&window_start__gte=&window_start__lt=&window_start__lte=", headers={'User-Agent': 'Mozilla/5.0'})
+    data = urlopen(req,timeout=10).read()
+    Rocketlaunch = json.loads(data.decode())
+    for i in Rocketlaunch["results"]:
+        spacecraft = i["rocket"]["configuration"]["full_name"]
+        type = i["launch_service_provider"]["type"]
+        time = i["net"]
+        company = i["launch_service_provider"]["name"]
+        pic = i["image"] 
+        mission = i["mission"]["name"]
+        description = i["mission"]["description"]
+        type = i["mission"]["type"]
+        orbit = i["mission"]["orbit"]["name"]
+ 
+        print(spacecraft)
+        print(type)
+        print(time)
+        print(company)
+        print(pic)
+        print(mission)
+        print(description)
+        print(type)
+        print(orbit)
+        print("\n")
 
-    print("\n--- Next five rocket launches ---\n\n")
-    try:
-        print(("Name : ") + str(Rocket.launch["launches"][0]["name"]) + str("\n"))
-    except:
-        print(("Name : ") + str("no name"))
-        pass
-    try:
-        print(
-            ("Description : ")
-            + str(Rocket.launch["launches"][0]["missions"][0]["description"])
-            + str("\n")
-        )
-    except:
-        print(("Description : ") + str("no description") + str("\n"))
-    try:
-        print(
-            ("Start time : ")
-            + str(Rocket.launch["launches"][0]["windowstart"])
-            + str("\n")
-        )
-    except:
-        print(("Start time : ") + str("no windowstart") + str("\n"))
-    print("\n\n")
-    try:
-        print(("Name : ") + str(Rocket.launch["launches"][1]["name"]) + str("\n"))
-    except:
-        print(("Name : ") + str("no name"))
-        pass
-    try:
-        print(
-            ("Description : ")
-            + str(Rocket.launch["launches"][1]["missions"][0]["description"])
-            + str("\n")
-        )
-    except:
-        print(("Description : ") + str("no description") + str("\n"))
-    try:
-        print(
-            ("Start time : ")
-            + str(Rocket.launch["launches"][1]["windowstart"])
-            + str("\n")
-        )
-    except:
-        print(("Start time : ") + str("no windowstart") + str("\n"))
-    print("\n\n")
-    try:
-        print(("Name : ") + str(Rocket.launch["launches"][2]["name"]) + str("\n"))
-    except:
-        print(("Name : ") + str("no name"))
-        pass
-    try:
-        print(
-            ("Description : ")
-            + str(Rocket.launch["launches"][2]["missions"][0]["description"])
-            + str("\n")
-        )
-    except:
-        print(("Description : ") + str("no description") + str("\n"))
-    try:
-        print(
-            ("Start time : ")
-            + str(Rocket.launch["launches"][2]["windowstart"])
-            + str("\n")
-        )
-    except:
-        print(("Start time : ") + str("no windowstart") + str("\n"))
-    print("\n\n")
-    try:
-        print(("Name : ") + str(Rocket.launch["launches"][3]["name"]) + str("\n"))
-    except:
-        print(("Name : ") + str("no name"))
-        pass
-    try:
-        print(
-            ("Description : ")
-            + str(Rocket.launch["launches"][3]["missions"][0]["description"])
-            + str("\n")
-        )
-    except:
-        print(("Description : ") + str("no description") + str("\n"))
-    try:
-        print(
-            ("Start time : ")
-            + str(Rocket.launch["launches"][3]["windowstart"])
-            + str("\n")
-        )
-    except:
-        print(("Start time : ") + str("no windowstart") + str("\n"))
-    print("\n\n")
-    try:
-        print(("Name : ") + str(Rocket.launch["launches"][4]["name"]) + str("\n"))
-    except:
-        print(("Name : ") + str("no name"))
-        pass
-    try:
-        print(
-            ("Description : ")
-            + str(Rocket.launch["launches"][4]["missions"][0]["description"])
-            + str("\n")
-        )
-    except:
-        print(("Description : ") + str("no description") + str("\n"))
-    try:
-        print(
-            ("Start time : ")
-            + str(Rocket.launch["launches"][4]["windowstart"])
-            + str("\n")
-        )
-    except:
-        print(("Start time : ") + str("no windowstart") + str("\n"))
-    print("\n\n")
-    print("Developer - Author : Hamdy Abou El Anein\nhttps://github.com/hamdyaea")
+    num = Rocketlaunch["count"]
+    print("Number of Rockets")
+    print(num)
 
 
 main()
